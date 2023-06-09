2352a4005cc1fe7c2ee9cd7c0836a4a92627968e
Phillip Logan <philo3194@gmail.com>
2023-06-08 23:04:48 -0500

    changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi import FastAPI,HTTPException,Request
 from datetime import datetime
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,13 +23,30 @@ app.add_middleware(
 @app.post("/test")
 async def test(request:Request):
     #receive a json object
-    file = open('data.csv', 'a', newline='')
-    writer = csv.writer(file)
-    start = timer()
+    #file = open('data.csv', 'a', newline='')
+    #writer = csv.writer(file)
+    #start = timer()
     test = await request.json()
-    end = timer()
-    writer.writerow([end-start])
-    print(end - start)
+    #end = timer()
+    #writer.writerow([end-start])
+    #print(end - start)
+
+@app.websocket("/testt")
+async def test(websocket: WebSocket):
+    #file = open('data.csv', 'w', newline='')
+    #writer = csv.writer(file)  
+    #start = timer()
+    await websocket.accept()
+    #end = timer()
+    #print(end - start)    
+    while True:
+        #receive a json object
+        #start = timer()
+        data = await websocket.receive_json()
+        print(data)
+        #end = timer()
+        #writer.writerow([end-start])
+        #print(end - start)
 
 if __name__ == "__main__":
     uvicorn.run("app:app")
